e28e855f9e7e9e22902f40a6c8b5cae3ac88edff
snapsid <siddharthchopra555@gmail.com>
2021-04-27 19:42:55 +0530

    ip screen added

---
+++ app/developer_assistant/developer_assistant/lib/ip.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyIp extends StatefulWidget {
+  @override
+  _MyIpState createState() => _MyIpState();
+}
+
+class _MyIpState extends State<MyIp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Developer Assistant'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Container(
+          margin: EdgeInsets.only(left: 40, right: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    hintText: "Enter your IP",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.teal,
+                elevation: 5,
+                child: MaterialButton(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {},
+                  minWidth: 130,
+                  height: 40,
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
