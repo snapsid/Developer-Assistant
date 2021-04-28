b32ffc237d2a5f9abfa64d50b8258338526136c2
snapsid <siddharthchopra555@gmail.com>
2021-04-29 00:18:00 +0530

    connected to python script

--- app/developer_assistant/developer_assistant/lib/main.dart
+++ app/developer_assistant/developer_assistant/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'python',
+    initialRoute: 'home',
     routes: {
       'ip': (context) => MyIp(),
       'home': (context) => MyHome(),
--- app/developer_assistant/developer_assistant/lib/python.dart
+++ app/developer_assistant/developer_assistant/lib/python.dart
@@ -1,5 +1,6 @@
 import 'package:code_text_field/code_text_field.dart';
 import 'package:developer_assistant/ip.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:highlight/languages/dart.dart';
@@ -14,17 +15,42 @@ class MyPython extends StatefulWidget {
 
 class _MyPythonState extends State<MyPython> {
   String ip;
-  var cmd;
+
   var _codeController;
   @override
   Widget build(BuildContext context) {
     // final source = "void main() {\n    print(\"Hello, world!\");\n}";
     var source = "print('hello world')";
+    var finalCode = "heyyy";
     _codeController = CodeController(
       text: source,
       language: python,
       theme: atomOneDarkTheme,
     );
+
+    onrun() async {
+      var cmd = _codeController.text;
+      print(cmd);
+      ip = MyIp.ip_public;
+      // var url = Uri.parse('http://${ip}/cgi-bin/python/terminal.py');
+      var url = Uri.parse('http://192.168.1.12/cgi-bin/python/test.py?x=$cmd');
+      try {
+        var response = await http.get(url, headers: {
+          "Accept": "application/json",
+          "Access-Control_Allow_Origin": "*"
+        });
+        var code = response.statusCode;
+        print(code);
+        if (code == 200) {
+          var body = response.body;
+          print(body);
+        } else {
+          print('invalid IP');
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -40,52 +66,65 @@ class _MyPythonState extends State<MyPython> {
               })
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal,
+        onPressed: () {
+          onrun();
+        },
+        child: Icon(Icons.play_arrow),
+      ),
       body: SingleChildScrollView(
         child: Container(
+          alignment: Alignment.center,
           child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
+                // alignment: Alignment.center,
+                // height: MediaQuery.of(context).size.height * 0.8,
+
                 child: CodeField(
                   controller: _codeController,
                   textStyle: TextStyle(fontFamily: 'SourceCode'),
                 ),
               ),
-              TextField(
-                onChanged: (value) {
-                  cmd = value;
-                },
-                decoration: InputDecoration(
-                    filled: true,
-                    hintText: "Enter your code",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  print(cmd);
-                  ip = MyIp.ip_public;
-                  // var url = Uri.parse('http://${ip}/cgi-bin/python/terminal.py');
-                  var url = Uri.parse(
-                      'http://192.168.1.12/cgi-bin/python/test.py?x=$cmd');
-                  try {
-                    var response = await http.get(url, headers: {
-                      "Accept": "application/json",
-                      "Access-Control_Allow_Origin": "*"
-                    });
-                    var code = response.statusCode;
-                    print(code);
-                    if (code == 200) {
-                      var body = response.body;
-                      print(body);
-                    } else {
-                      print('invalid IP');
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
+              // Container(
+              //   child: CircleAvatar(
+              //     minRadius: 30,
+              //     backgroundColor: Colors.teal,
+              //     child: IconButton(
+              //       splashColor: Colors.teal,
+              //       focusColor: Colors.orange,
+              //       highlightColor: Colors.blue,
+              //       color: Colors.white,
+              //       icon: Icon(Icons.play_arrow),
+              //       onPressed: () async {
+              //         print(cmd);
+              //         ip = MyIp.ip_public;
+              //         // var url = Uri.parse('http://${ip}/cgi-bin/python/terminal.py');
+              //         var url = Uri.parse(
+              //             'http://192.168.1.12/cgi-bin/python/test.py?x=$cmd');
+              //         try {
+              //           var response = await http.get(url, headers: {
+              //             "Accept": "application/json",
+              //             "Access-Control_Allow_Origin": "*"
+              //           });
+              //           var code = response.statusCode;
+              //           print(code);
+              //           if (code == 200) {
+              //             var body = response.body;
+              //             print(body);
+              //           } else {
+              //             print('invalid IP');
+              //           }
+              //         } catch (e) {
+              //           print(e);
+              //         }
+              //       },
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ),
