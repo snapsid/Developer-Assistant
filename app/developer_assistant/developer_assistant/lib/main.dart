17ad4eb2fe8696d4fe5f30682c22a953fc19136a
snapsid <siddharthchopra555@gmail.com>
2021-04-28 19:39:47 +0530

    code editior added

--- app/developer_assistant/developer_assistant/lib/main.dart
+++ app/developer_assistant/developer_assistant/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'ip',
+    initialRoute: 'python',
     routes: {
       'ip': (context) => MyIp(),
       'home': (context) => MyHome(),
--- app/developer_assistant/developer_assistant/lib/python.dart
+++ app/developer_assistant/developer_assistant/lib/python.dart
@@ -1,6 +1,11 @@
+import 'package:code_text_field/code_text_field.dart';
 import 'package:developer_assistant/ip.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:highlight/languages/dart.dart';
+import 'package:highlight/languages/python.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
+import 'package:flutter_highlight/themes/atom-one-dark.dart';
 
 class MyPython extends StatefulWidget {
   @override
@@ -9,34 +14,70 @@ class MyPython extends StatefulWidget {
 
 class _MyPythonState extends State<MyPython> {
   String ip;
+  var cmd;
+  var _codeController;
   @override
   Widget build(BuildContext context) {
+    // final source = "void main() {\n    print(\"Hello, world!\");\n}";
+    final source = "print('hello world')";
+    _codeController = CodeController(
+      text: source,
+      language: python,
+      theme: atomOneDarkTheme,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Python Terminal'),
         backgroundColor: Colors.teal,
       ),
-      body: ElevatedButton(
-        onPressed: () async {
-          ip = MyIp.ip_public;
-          var url = Uri.parse('http://${ip}/cgi-bin/python/terminal.py');
-          try {
-            var response = await http.get(url, headers: {
-              "Accept": "application/json",
-              "Access-Control_Allow_Origin": "*"
-            });
-            var code = response.statusCode;
-            print(code);
-            if (code == 200) {
-              var body = response.body;
-              print(body);
-            } else {
-              print('invalid IP');
-            }
-          } catch (e) {
-            print(e);
-          }
-        },
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              child: CodeField(
+                controller: _codeController,
+                textStyle: TextStyle(fontFamily: 'SourceCode'),
+              ),
+            ),
+            TextField(
+              onChanged: (value) {
+                cmd = value;
+              },
+              decoration: InputDecoration(
+                  filled: true,
+                  hintText: "Enter your code",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                print(cmd);
+                ip = MyIp.ip_public;
+                // var url = Uri.parse('http://${ip}/cgi-bin/python/terminal.py');
+                var url = Uri.parse(
+                    'http://192.168.1.12/cgi-bin/python/test.py?x=$cmd');
+                try {
+                  var response = await http.get(url, headers: {
+                    "Accept": "application/json",
+                    "Access-Control_Allow_Origin": "*"
+                  });
+                  var code = response.statusCode;
+                  print(code);
+                  if (code == 200) {
+                    var body = response.body;
+                    print(body);
+                  } else {
+                    print('invalid IP');
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
