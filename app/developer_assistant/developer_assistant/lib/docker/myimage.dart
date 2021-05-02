11b8822f5f7cedddf4f6200cbcd2e978b7ba8606
snapsid <siddharthchopra555@gmail.com>
2021-05-02 11:44:44 +0530

    getting image name in console

--- app/developer_assistant/developer_assistant/lib/docker/myimage.dart
+++ app/developer_assistant/developer_assistant/lib/docker/myimage.dart
@@ -1,4 +1,8 @@
+import 'package:developer_assistant/ip.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class MyDockerImage extends StatefulWidget {
   @override
@@ -6,12 +10,86 @@ class MyDockerImage extends StatefulWidget {
 }
 
 class _MyDockerImageState extends State<MyDockerImage> {
+  bool loading = false;
+
+  String ip;
+
+  @override
+  void initState() {
+    ip = MyIp.ip_public;
+    // TODO: implement initState
+    super.initState();
+    getImages();
+  }
+
+  myToast(mymsg, color) {
+    Fluttertoast.showToast(
+        msg: mymsg,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 2,
+        backgroundColor: color,
+        textColor: Colors.white,
+        webPosition: "center",
+        fontSize: 16.0);
+  }
+
+  getImages() async {
+    myToast('Please Wait...', Colors.blue);
+    setState(() {
+      loading = true;
+    });
+    var url = Uri.parse('http://${ip}/cgi-bin/docker/docker/myimage.py');
+    try {
+      var response = await http.get(url, headers: {
+        "Accept": "application/json",
+        "Access-Control_Allow_Origin": "*"
+      });
+      var code = response.statusCode;
+      print(code);
+      if (code == 200) {
+        var body = await response.body;
+        print(body);
+        myToast(body, Colors.teal);
+        setState(() {
+          loading = false;
+        });
+      } else {
+        print('invalid IP');
+        myToast('Invalid IP', Colors.red);
+        setState(() {
+          loading = false;
+        });
+        // Navigator.pushNamedAndRemoveUntil(
+        //     context, 'dockerhome', (route) => false);
+      }
+    } catch (e) {
+      print(e);
+      myToast('Server connection failed...', Colors.red);
+      setState(() {
+        loading = false;
+      });
+      // Navigator.pushNamedAndRemoveUntil(
+      //     context, 'dockerhome', (route) => false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Docker my images'),
         backgroundColor: Colors.teal,
+        actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})],
+      ),
+      body: ModalProgressHUD(
+        inAsyncCall: loading,
+        progressIndicator: CircularProgressIndicator(
+          // backgroundColor: Colors.teal,
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
+          strokeWidth: 6,
+        ),
+        child: Container(),
       ),
     );
   }
