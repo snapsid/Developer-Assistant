a3e21e3890c0a6dfb63de8f63c238335a1a92dfe
snapsid <siddharthchopra555@gmail.com>
2021-05-02 10:59:12 +0530

    run container ui added

--- app/developer_assistant/developer_assistant/lib/docker/runcontainer.dart
+++ app/developer_assistant/developer_assistant/lib/docker/runcontainer.dart
@@ -1,4 +1,7 @@
+import 'package:developer_assistant/ip.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class DockerRunContainer extends StatefulWidget {
   @override
@@ -6,12 +9,134 @@ class DockerRunContainer extends StatefulWidget {
 }
 
 class _DockerRunContainerState extends State<DockerRunContainer> {
+  bool loading = false;
+  var image = "";
+  var version = "";
+  String ip;
+
+  @override
+  void initState() {
+    ip = MyIp.ip_public;
+    // TODO: implement initState
+    super.initState();
+  }
+
+  myToast(mymsg, color) {
+    Fluttertoast.showToast(
+        msg: mymsg,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 2,
+        backgroundColor: color,
+        textColor: Colors.white,
+        webPosition: "center",
+        fontSize: 16.0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Docker launch container'),
         backgroundColor: Colors.teal,
+      ),
+      body: ModalProgressHUD(
+        inAsyncCall: loading,
+        progressIndicator: CircularProgressIndicator(
+          // backgroundColor: Colors.teal,
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
+          strokeWidth: 6,
+        ),
+        child: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  cursorColor: Colors.teal,
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.teal.shade600),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      filled: true,
+                      hintText: "Give Container Name",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal),
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  cursorColor: Colors.teal,
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.teal.shade600),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      filled: true,
+                      hintText: "Enter Image Name",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal),
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  cursorColor: Colors.teal,
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.teal.shade600),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      filled: true,
+                      hintText: "Enter Version (Default: latest)",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal),
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Material(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.teal,
+                  elevation: 5,
+                  child: MaterialButton(
+                    child: Text(
+                      'Launch Container',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {},
+                    minWidth: 130,
+                    height: 40,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
